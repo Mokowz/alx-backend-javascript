3400b056baf2b3ff6d685a717ae8ce68d3a64f73
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-22 11:53:11 +0300

    Iterate over the report

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,21 @@
 export default function createIteratorObject(report) {
+  const employees = Object.values(report.allEmployees).reduce((a, b) => {
+    a.push(...b);
+    return a;
+  }, []);
 
+  let indx = 0;
+  const maxIndx = employees.length;
+
+  return {
+    next() {
+      if (indx < maxIndx) {
+        const result = { value: employees[indx], done: false };
+        indx += 1;
+        return result;
+      }
+      return { value: null, done: true };
+    },
+    [Symbol.iterator]: () => this.next(),
+  };
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
+  let i = reportWithIterator.next();
+  let lst = '';
 
+  while (!i.done) {
+    lst += `${i.value} | `;
+    i = reportWithIterator.next();
+  }
+  return lst.slice(0, lst.length - 3);
 }
