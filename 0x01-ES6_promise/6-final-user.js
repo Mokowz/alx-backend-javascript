15513a7ccf6107e60d49bb6a0c6aa4b66dc67e43
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-23 11:02:35 +0300

    Run async function

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+
+  const lst = [];
+
+  try {
+    const user = await signUpUser(firstName, lastName);
+    lst.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    lst.push({ status: 'rejected', value: `Error: ${err} `});
+  }
+
+  return lst;
+}
