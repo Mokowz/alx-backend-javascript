aafe4555a94a5ca35385e5b9c3abee4bba9350e4
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-25 11:19:16 +0300

    Make changes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const lst = [];
-
   try {
     const user = await signUpUser(firstName, lastName);
     lst.push({ status: 'fulfilled', value: user });
     await uploadPhoto(fileName);
   } catch (err) {
-    lst.push({ status: 'rejected', value: `Error: ${err} ` });
+    lst.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
   }
-
   return lst;
 }
