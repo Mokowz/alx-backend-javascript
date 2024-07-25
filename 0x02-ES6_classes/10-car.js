aafb331ef643dd3bc32927400af8a0d7adfb0172
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-25 12:59:37 +0300

    Fix code issues

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') throw new Error();
     this._brand = brand;
     this._motor = motor;
     this._color = color;
