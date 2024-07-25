f39330e9da9695cc3c253ec5b29a7aa861bf9c72
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-25 12:51:42 +0300

    PAss in previous class function for privacy

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') throw new Error();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
