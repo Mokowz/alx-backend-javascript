67ee6c34f9a5867ce83f23ec0f3ac471b1d88a0b
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-07-30 10:58:05 +0300

    Fix lint issues

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,19 @@
 export default function getListStudents() {
   const lst = [];
-  
   lst.push({
     id: 1,
     firstName: 'Guillaume',
-    location: 'San Francisco'
+    location: 'San Francisco',
   });
   lst.push({
     id: 2,
     firstName: 'James',
-    location: 'Columbia'
+    location: 'Columbia',
   });
   lst.push({
     id: 5,
     firstName: 'Serena',
-    location: 'San Francisco'
+    location: 'San Francisco',
   });
 
   return lst;
